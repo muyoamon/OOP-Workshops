d67a42bffb9d526a80c2cbb987e4f7cec0f59ad1
muyoamon <muyoamon@gmail.com>
2024-01-23 12:54:03 -0500

    WS02 lab

---
+++ WS02/lab/Assessment.cpp
@@ -0,0 +1,70 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include "Assessment.h"
+namespace seneca {
+  bool read(int& value, FILE* fptr)
+  {
+    return fscanf(fptr, "%d", &value) != EOF;
+  }
+
+  bool read(double& value, FILE* fptr)
+  {
+    return fscanf(fptr, "%lf", &value) != EOF;
+  }
+
+  bool read(char* cstr, FILE* fptr)
+  {
+    return fscanf(fptr, ",%60[^\n]\n", cstr) != EOF;
+  }
+
+  bool read(Assessment& assess, FILE* fptr)
+  {
+    double mark;
+    char title[60+1];
+    int length;
+    bool ret = false;
+    if (read(mark, fptr) && read(title, fptr))
+    {
+      length = strlen(title);
+      assess.m_mark = new double(mark);
+      assess.m_title = new char[length+1];
+      strcpy(assess.m_title, title);
+      ret = true;
+    }
+    return ret;
+  }
+
+  void freeMem(Assessment*& aptr, int size) 
+  {
+    int i;
+    for (i=0;i<size;i++)
+    {
+      delete aptr[i].m_title;
+      delete aptr[i].m_mark;
+    }
+    delete aptr;
+  }
+
+  int read(Assessment*& aptr, FILE* fptr)
+  {
+    int sSize, cSize = 0;
+    if (read(sSize,fptr))
+    {
+      aptr = new Assessment[sSize];
+      while (read(aptr[cSize],fptr) && (cSize < sSize))
+      {
+        cSize++;
+      }
+      if (cSize != sSize)
+      {
+        freeMem(aptr, cSize);
+        cSize = 0;
+      }
+    }
+    return cSize;
+
+  }
+
+  
+}
--- WS02/lab/Assessment.h
+++ WS02/lab/Assessment.h
@@ -1,1 +1,49 @@
+#ifndef SENECA_ASSESSMENT_H
+#define SENECA_ASSESSMENT_H
 
+#include <cstdio>
+
+namespace seneca {
+struct Assessment {
+  double *m_mark;
+  char *m_title;
+};
+
+/// Reads one integer
+/// @param value variable to store
+/// @param fptr pointer to file
+/// @return true if success, false otherwise.
+bool read(int& value, FILE* fptr);
+
+/// Reads one double
+/// @param value variable to store
+/// @param fptr pointer to file
+/// @return true if success, false otherwise.
+bool read(double& value, FILE* fptr);
+
+/// Skip a comma, reads string no longer than 60 characters
+/// @param value variable to store
+/// @param fptr pointer to file
+/// @return true if success, false otherwise.
+bool read(char* cstr, FILE* fptr);
+
+/// allocate dynamic double and cstring into field of Assessment structure
+/// @param assess Assessment variable
+/// @param fptr pointer to file
+/// @return true if success, false otherwise.
+bool read(Assessment& assess, FILE* fptr);
+
+/// free memory of assessment allocated field
+/// @param aptr pointer to dynamic Assessment array
+/// @param fptr pointer to file
+void freeMem(Assessment*& aptr, int size);
+
+/// Allocate dyanmic Assessment array according to file
+/// @param assess Assessment variable
+/// @param fptr pointer to file
+/// @return size of Assessment array if number matches,
+/// otherwise return zero
+int read(Assessment*& aptr, FILE* fptr);
+
+} // namespace seneca
+#endif // !SENECA_ASSESSMENT_H
