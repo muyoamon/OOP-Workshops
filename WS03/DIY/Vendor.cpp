4c92bf144d00e70db7f983412cedcf803004109e
muyoamon <muyoamon@gmail.com>
2024-02-06 14:12:32 -0500

    add header & reflect.txt

--- WS03/DIY/Vendor.cpp
+++ WS03/DIY/Vendor.cpp
@@ -1,3 +1,10 @@
+// Name: Thanatorn Wongthanaporn
+// email: twongthanaporn@myseneca.ca
+// id: 161197223
+// Date: 6/2/2024
+//
+//I have done all the coding by myself and only copied the code 
+//that my professor provided to complete my workshops and assignments.
 #include "Vendor.h"
 #include "Icecream.h"
 #include <iostream>
