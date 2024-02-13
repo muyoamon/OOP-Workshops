cae43a0edf66baa915bcd459a8048015cbc2a528
muyoamon <muyoamon@gmail.com>
2024-02-13 11:06:47 -0500

    WS04 DIY

--- WS04/DIY/Guitar.cpp
+++ WS04/DIY/Guitar.cpp
@@ -13,23 +13,90 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// email: twongthanaporn@myseneca.ca
+// id: 161197223
+// Date: 13/1/2024
+//
+// I have done all the coding by myself and only copied the code
+// that my professor provided to complete my workshops and assignments.
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstring>
 #include "Guitar.h"
+#include <cstring>
+#include <iostream>
 using namespace std;
 
-namespace seneca{
-   const char* GuitarStr::material() const {
-      return m_material;
-   }
-   double GuitarStr::gauge() const {
-      return m_gauge;
-   }
-   GuitarStr::GuitarStr() {};
+namespace seneca {
+const char *GuitarStr::material() const { return m_material; }
+double GuitarStr::gauge() const { return m_gauge; }
+GuitarStr::GuitarStr(){};
 
-   GuitarStr::GuitarStr(const char* ma, double ga) {
-      strncpy(m_material, ma, MAT_LEN);
-      m_gauge = ga;
-   };
+GuitarStr::GuitarStr(const char *ma, double ga) {
+  strncpy(m_material, ma, MAT_LEN);
+  m_gauge = ga;
+};
+
+bool GuitarStr::operator==(const GuitarStr &rhs) const {
+  return this->m_gauge == rhs.m_gauge &&
+         !strcmp(this->m_material, rhs.m_material);
 }
+
+Guitar::Guitar(const char *mod) {
+  this->m_strings = nullptr;
+  this->m_numStrings = 0;
+  strncpy(this->m_model, mod, strlen(mod) + 1);
+}
+Guitar::Guitar(GuitarStr strs[], int ns, const char *mod) {
+  this->m_numStrings = ns;
+  this->m_strings = new GuitarStr[ns];
+  strncpy(this->m_model, mod, strlen(mod) + 1);
+  for (int i = 0; i < ns; i++) {
+    this->m_strings[i] = strs[i];
+  }
+}
+Guitar::~Guitar() { delete[] this->m_strings; }
+
+bool Guitar::changeString(const GuitarStr &gs, int sn) {
+  return sn < this->m_numStrings ? (this->m_strings[sn] = gs) == this->m_strings[sn] : 0;
+}
+void Guitar::reString(GuitarStr strs[], int ns) {
+  delete[] this->m_strings;
+  this->m_strings = new GuitarStr[ns];
+  this->m_numStrings = ns;
+  for (int i = 0; i < ns; i++) {
+    this->m_strings[i] = strs[i];
+  }
+}
+void Guitar::deString() {
+  delete[] this->m_strings;
+  this->m_numStrings = 0;
+  this->m_strings = nullptr;
+}
+bool Guitar::strung() const {
+  return this->m_strings != nullptr;
+}
+bool Guitar::matchGauge(double ga) const {
+  bool match = false;
+  for (int i=0;i<this->m_numStrings;i++) {
+    if (this->m_strings[i].gauge() == ga) match=true;
+  }
+  return match;
+}
+std::ostream &Guitar::display(std::ostream &os) const {
+  if (!strcmp(this->m_model, "") || this->m_numStrings == 0) {
+    os << "***Empty Guitar***" << endl;
+  } else {
+    os << "Guitar Model: " << this->m_model << endl;
+    os << "Strings: " << this->m_numStrings << endl;
+    if (this->strung()) {
+      for (int i=0;i<this->m_numStrings;i++) {
+        os << "#" << i+1;
+        os.width(MAT_LEN);
+        os.precision(1);
+        os << this->m_strings[i].material() << " | " << this->m_strings[i].gauge() << endl;
+      }
+    }
+  }
+  return os;
+}
+} // namespace seneca
--- WS04/DIY/Guitar.h
+++ WS04/DIY/Guitar.h
@@ -13,6 +13,14 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// email: twongthanaporn@myseneca.ca
+// id: 161197223
+// Date: 13/2/2024
+//
+// I have done all the coding by myself and only copied the code
+// that my professor provided to complete my workshops and assignments.
+#include <ostream>
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef SENECA_GUITAR_H
 #define SENECA_GUITAR_H
@@ -22,7 +30,7 @@
 namespace seneca{
 
   const int MAT_LEN = 10;
-
+  const int MOD_LEN = 15;
   class GuitarStr {
      char m_material[MAT_LEN]{};
      double m_gauge{};
@@ -31,6 +39,22 @@ namespace seneca{
      double gauge()const;
      GuitarStr();
      GuitarStr(const char* ma, double ga);
+     bool operator==(const GuitarStr& rhs) const;
+  };
+  class Guitar {
+    GuitarStr* m_strings;
+    int m_numStrings;
+    char m_model[MOD_LEN + 1];
+  public:
+    Guitar(const char* mod="Stratocaster");
+    Guitar(GuitarStr strs[], int ns, const char* mod);
+    ~Guitar();
+    bool changeString(const GuitarStr& gs, int sn);
+    void reString(GuitarStr strs[], int ns);
+    void deString();
+    bool strung() const;
+    bool matchGauge(double ga) const;
+    std::ostream& display(std::ostream& os = std::cout) const;
   };
 
 }
