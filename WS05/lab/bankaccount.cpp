4c8ef1998fcfbd8222ed589d77d923aa79e1d7d8
muyoamon <muyoamon@gmail.com>
2024-02-16 21:36:18 -0500

    WS05 Lab

--- WS05/lab/bankaccount.cpp
+++ WS05/lab/bankaccount.cpp
@@ -10,47 +10,162 @@
 // -----------------------------------------------------------
 // Name                 Date            Reason
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// Email: twongthanaporn@myseneca.ca 
+// id: 161197223
+// Date: 16/2/2024
+
+// I have done all the coding by myself and only copied the code that my
+// professor provided to complete my workshops and assignments.
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "bankaccount.h"
-#include <iostream>
 #include <cstring>
-#include <cstdio>
+#include <iostream>
 
 using namespace std;
 
-namespace seneca
-{
-	// Default Constructor with default arguments
-	bankAccount::bankAccount(const char* name, bool checking)
-	{
-		m_userName[0] = '\0';		// Empty string implies is account is not open 
-		setup(name, checking);
-	}
-
-	// Open bank account: setup can only be done once!
-	bool bankAccount::setup(const char* name, bool checking)
-	{
-		if (isOpen())					
-			return false;
-		if (name == nullptr)
-			return false;
-
-		strcpy(m_userName, name);	
-		m_monthlyTransactions = 0;	
-		m_checking = checking;
-		m_funds = 0;
-		return true;
-	}
-
-	// Account is open if user name string is not empty
-	bool bankAccount::isOpen() const
-	{
-		return (m_userName[0] != '\0');
-	}
-
-	// ADD: Type Conversion, Unary & Binary Operators; + Additional Methods
-
-	// ADD: Global Helper Functions
-
+namespace seneca {
+// Default Constructor with default arguments
+bankAccount::bankAccount(const char *name, bool checking) {
+  m_userName[0] = '\0'; // Empty string implies is account is not open
+  setup(name, checking);
 }
+
+// Open bank account: setup can only be done once!
+bool bankAccount::setup(const char *name, bool checking) {
+  if (isOpen())
+    return false;
+  if (name == nullptr)
+    return false;
+
+  strcpy(m_userName, name);
+  m_monthlyTransactions = 0;
+  m_checking = checking;
+  m_funds = 0;
+  return true;
+}
+
+// Account is open if user name string is not empty
+bool bankAccount::isOpen() const { return (m_userName[0] != '\0'); }
+
+// ADD: Type Conversion, Unary & Binary Operators; + Additional Methods
+
+bankAccount::operator bool() const { return this->isOpen(); }
+
+bankAccount::operator double() const { return this->m_funds; }
+
+bankAccount &bankAccount::operator++() {
+  if (this->isOpen() && this->m_funds > 0.) {
+    switch ((int)this->m_checking) {
+    case 1:
+      this->m_funds *= 1 + CHECK_INTEREST / 100;
+      break;
+    case 0:
+      this->m_funds *= 1 + SAVING_INTEREST / 100;
+      break;
+    default:
+      break;
+    }
+  }
+  return *this;
+}
+
+bankAccount &bankAccount::operator--() {
+  if (this->isOpen()) {
+    switch ((int)this->m_checking) {
+    case 1:
+      this->m_funds -= CHECK_TRS_FEE * this->m_monthlyTransactions;
+      break;
+    case 0:
+      this->m_funds -= SAVING_TRS_FEE * this->m_monthlyTransactions;
+      break;
+    default:
+      break;
+    }
+  }
+  return *this;
+}
+
+bool bankAccount::operator+=(double value) {
+  bool success = false;
+  if ((success = this->isOpen())) {
+    this->m_funds += value;
+    this->m_monthlyTransactions++;
+    cout.precision(2);
+    cout.setf(cout.fixed);
+    cout << "Deposit $" << value << " for " << this->m_userName << endl;
+    cout.unsetf(cout.fixed);
+  }
+  return success;
+}
+
+bool bankAccount::operator-=(double value) {
+  bool success = this->isOpen();
+  if (success) {
+    this->m_funds -= value;
+    this->m_monthlyTransactions++;
+    cout.precision(2);
+    cout.setf(cout.fixed);
+    cout << "Withdraw $" << value << " for " << this->m_userName << endl;
+    cout.unsetf(cout.fixed);
+  }
+  return success;
+}
+
+bool bankAccount::operator==(const bankAccount &rhs) const {
+  return !strcmp(this->m_userName, rhs.m_userName) &&
+         this->m_funds == rhs.m_funds && this->m_checking == rhs.m_checking;
+}
+
+bool bankAccount::operator>(double rhs) const { return this->m_funds > rhs; }
+
+bool bankAccount::operator<=(double rhs) const {
+  return !(this->m_funds > rhs);
+}
+
+bool bankAccount::operator<<(bankAccount &rhs) {
+  bool success = false;
+  double funds = 0.;
+  if ((success = this->isOpen() && rhs.isOpen())) {
+    funds = rhs;
+    cout.precision(2);
+    cout.setf(cout.fixed);
+    cout << "Transfer $" << funds << " from " << rhs.m_userName << " to "
+         << this->m_userName << endl;
+    cout.unsetf(cout.fixed);
+    *this += funds;
+    rhs -= funds;
+  }
+  return success;
+}
+
+void bankAccount::display(void) const {
+  char backupFill;
+  cout << "Display Account -> User:";
+  cout.width(ACCT_MAXLEN_USER);
+  backupFill = cout.fill('-');
+  if (this->isOpen()) {
+    cout << this->m_userName << " | ";
+    cout.fill(backupFill);
+    cout.width(8);
+    cout << (this->m_checking ? "Checking" : "Savings") << " | Balance: $";
+    cout.width(8);
+    cout.precision(2);
+    cout.setf(cout.fixed);
+    cout << this->m_funds << " | Transactions:";
+    cout.unsetf(cout.fixed);
+    cout.fill('0');
+    cout.width(3);
+    cout << this->m_monthlyTransactions << endl;
+  } else {
+    cout << " NOT OPEN" << endl;
+  }
+  cout.fill(backupFill);
+}
+// ADD: Global Helper Functions
+
+bool operator>(double lhs, const bankAccount &rhs) { return rhs <= lhs; }
+bool operator<=(double lhs, const bankAccount &rhs) { return rhs > lhs; }
+
+} // namespace seneca
--- WS05/lab/bankaccount.h
+++ WS05/lab/bankaccount.h
@@ -10,38 +10,59 @@
 // -----------------------------------------------------------
 // Name                 Date            Reason
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// Email: twongthanaporn@myseneca.ca 
+// id: 161197223
+// Date: 16/2/2024
+
+// I have done all the coding by myself and only copied the code that my
+// professor provided to complete my workshops and assignments.
 #pragma once
 #ifndef SENECA_BANKACCOUNT_H_
 #define SENECA_BANKACCOUNT_H_
 
+namespace seneca {
+const int ACCT_MAXLEN_USER = 16; // Max Length of user name
+const double CHECK_TRS_FEE = 1.25;
+const double CHECK_INTEREST = .5; // in percentage
+const double SAVING_TRS_FEE = 3.5;
+const double SAVING_INTEREST = 2.5; // in percentage
+// Class Definition
+class bankAccount {
 
+private:
+  // Data Members
+  char m_userName[ACCT_MAXLEN_USER];
+  double m_funds;
+  bool m_checking;
+  int m_monthlyTransactions;
 
-namespace seneca
-{
-   const int	 ACCT_MAXLEN_USER = 16;					// Max Length of user name
+  bool isOpen() const;
 
-   // Class Definition
-   class bankAccount
-   {
+public:
+  bankAccount(const char *name = nullptr, bool checking = false);
+  bool setup(const char *name, bool checking);
 
-   private:
-      // Data Members
-      char	m_userName[ACCT_MAXLEN_USER];
-      double	m_funds;
-      bool	m_checking;
-      int		m_monthlyTransactions;
+  operator bool() const;
+  operator double() const;
 
-      bool isOpen() const;
-   public:
-      bankAccount(const char* name = nullptr, bool checking = false);
-      bool setup(const char* name, bool checking);
+  bankAccount &operator++();
+  bankAccount &operator--();
 
-      // ADD: Type Conversion, Unary, Binary Operators & Additional Methods
-      // ...
-   };
+  bool operator+=(double);
+  bool operator-=(double);
 
-   // ADD: Global helpers
-   // ...
-}
+  bool operator==(const bankAccount &) const;
+  bool operator>(double) const;
+  bool operator<=(double) const;
+
+  bool operator<<(bankAccount &);
+
+  void display(void) const;
+};
+
+bool operator>(double lhs, const bankAccount &rhs);
+bool operator<=(double lhs, const bankAccount &rhs);
+
+} // namespace seneca
 #endif
-
