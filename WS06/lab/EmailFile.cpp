73aea9786d55c350c2721d1c713d4c2ee39891e7
muyoamon <muyoamon@gmail.com>
2024-03-08 21:31:21 -0500

    WS6 Lab

--- WS06/lab/EmailFile.cpp
+++ WS06/lab/EmailFile.cpp
@@ -13,6 +13,13 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// Email: twongthanaporn@myseneca.ca 
+// id: 161197223
+// Date: 8/3/2024
+
+// I have done all the coding by myself and only copied the code that my
+// professor provided to complete my workshops and assignments.
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <fstream>
@@ -141,9 +148,121 @@ namespace seneca
             return ostr;
     }
 
+    void EmailFile::loadEmails() {
+        ifstream file(m_filename);
+        int i=0;
+        if (file) {
+            delete[] m_emailLines;
+            m_emailLines = new Email[m_noOfEmails];
+            for (i = 0; i < m_noOfEmails && !file.eof(); i++) {
+                if (!m_emailLines[i].load(file)) {
+                    i--;
+                    break;
+                }
+            }
+            m_noOfEmails = i + 1;
+        }
+        file.close();
+    }
+
+    EmailFile::operator bool() const {
+        return m_emailLines == nullptr && m_noOfEmails == 0 && m_filename == nullptr;
+    }
+
+    EmailFile::EmailFile() {
+        m_filename = nullptr;
+        m_noOfEmails = 0;
+        m_emailLines = nullptr;
+    }
+
+    EmailFile::EmailFile(const char* filename) {
+        if (filename) {
+            setFilename(filename);
+            setNoOfEmails();
+            loadEmails();
+        }
+        else {
+            m_noOfEmails = 0;
+            m_emailLines = nullptr;
+            m_filename = nullptr;
+        }
+    }
+
+    EmailFile::EmailFile(const EmailFile& source) {
+        m_filename = nullptr;
+        m_emailLines = nullptr;
+        (*this) = source;
+    }
+
+    EmailFile::~EmailFile() {
+        delete[] m_filename;
+        delete[] m_emailLines;
+    }
+
+    EmailFile& EmailFile::operator=(const EmailFile& source) {
+        delete[] m_filename;
+        delete[] m_emailLines;
+        if (source.m_filename) {
+            setFilename(source.m_filename);
+            m_noOfEmails = source.m_noOfEmails;
+            m_emailLines = new Email[m_noOfEmails];
+            for (int i = 0; i < m_noOfEmails; i++) {
+                m_emailLines[i] = source.m_emailLines[i];
+            }
+        }
+        else {
+            m_noOfEmails = 0;
+            m_emailLines = nullptr;
+            m_filename = nullptr;
+        }
+        return *this;
+    }
+
+    bool EmailFile::saveToFile(const char* filename) const {
+        bool result;
+        ofstream file(filename);
+        if (result = file.is_open()) {
+            for (int i = 0; i < m_noOfEmails; i++) {
+                file << m_emailLines[i].m_email << "," << m_emailLines[i].m_name << "," << m_emailLines[i].m_year << endl;
+            }
+        }
+        else {
+            cout << "Error: Could not open file: " << filename << endl;
+            result = 0;
+        }
+        file.close();
+        return result;
+    }
+    void EmailFile::fileCat(const EmailFile& obj, const char* name) {
+        int newNoOfEmails = 0;
+        Email* newEmailLines = nullptr;
+        if (this->valid() && obj.valid()) {
+            newNoOfEmails = this->m_noOfEmails + obj.m_noOfEmails;
+            newEmailLines = new Email[newNoOfEmails];
+            for (int i = 0; i < this->m_noOfEmails; i++) {
+                newEmailLines[i] = this->m_emailLines[i];
+            }
+            for (int i = 0; i < obj.m_noOfEmails; i++) {
+                newEmailLines[i + this->m_noOfEmails] = obj.m_emailLines[i];
+            }
+            this->m_noOfEmails = newNoOfEmails;
+            delete[] this->m_emailLines;
+            this->m_emailLines = newEmailLines;
+            if (name) {
+                this->setFilename(name);
+            }
+            this->saveToFile(this->m_filename);
+        }
+    }
+
+    bool EmailFile::valid() const {
+        return !(m_filename == nullptr || m_emailLines == nullptr || m_noOfEmails == 0);
+    }
+
     std::ostream& operator<<(std::ostream& ostr, const EmailFile& text)
     {
         return text.view(ostr);
     }
 
+    
 }
--- WS06/lab/EmailFile.h
+++ WS06/lab/EmailFile.h
@@ -13,6 +13,13 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+// Name: Thanatorn Wongthanaporn
+// Email: twongthanaporn@myseneca.ca 
+// id: 161197223
+// Date: 8/3/2024
+
+// I have done all the coding by myself and only copied the code that my
+// professor provided to complete my workshops and assignments.
 #ifndef SENECA_EMAILFILE_H
 #define SENECA_EMAILFILE_H
 #include <iostream>
@@ -32,9 +39,9 @@ namespace seneca{
 		bool load(std::ifstream& in);
 		~Email();
 		// Prohibit the copy constructor for this class: 
-
+		Email(const Email&) = delete;
 		// Make the EmailFile class a "friend" of the Email class
-
+		friend class EmailFile;
 	};
 
 	
@@ -42,14 +49,30 @@ namespace seneca{
 	{
 		Email* m_emailLines{ nullptr };
 		char* m_filename{};
-		int m_noOfEmails{ 0 }; 
+		int m_noOfEmails{ 0 };
+
 		void setFilename(const char* filename);
 		void setEmpty();
 		bool setNoOfEmails();
+
+		void loadEmails();
+		operator bool() const;
 		
 	public:
+
+		EmailFile();
+		EmailFile(const char* filename);
+		EmailFile(const EmailFile& source);
+		~EmailFile();
+
+		EmailFile& operator=(const EmailFile& source);
 		
 		std::ostream& view(std::ostream& ostr) const;
+
+		bool saveToFile(const char* filename) const;
+		void fileCat(const EmailFile& obj, const char* name = nullptr);
+		bool valid() const;
+
 	};
 
 	std::ostream& operator<<(std::ostream& ostr, const EmailFile& text);
